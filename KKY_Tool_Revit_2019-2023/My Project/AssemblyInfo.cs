37124d387d3d9f6e54f2faf67b276b48a871387c
zerokky11 <kkykiki89@nate.com>
2026-02-20 13:03:58 +0900

    Convert My Project designer/assembly metadata files to C#

---
+++ KKY_Tool_Revit_2019-2023/My Project/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KKY_Tool_Revit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KKY_Tool_Revit")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5c4a0359-4144-4af6-879c-7a4c44673dd0")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
